51282c2a8ed5bc70a93338ce29e98a2a36318ccf
Ana Artagaveytia <anaartagaveytia1@gmail.com>
2026-04-25 00:37:36 -0300

    update app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
   const isMobile = useMobile()
 
   useEffect(() => {
-    if (isMobile) {
+    if (isMobile && window.location.pathname === "/") {
       router.replace("/app-home")
-    } else {
+    } else if (!isMobile && window.location.pathname === "/") {
       router.replace("/planos")
     }
   }, [isMobile])
