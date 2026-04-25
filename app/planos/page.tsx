46ff5238d1e1aca460107b3de4a80551753198b7
Ana Artagaveytia <anaartagaveytia1@gmail.com>
2026-04-25 00:17:55 -0300

    add app/planos/page.tsx

---
+++ app/planos/page.tsx
@@ -0,0 +1,7 @@
+export default function Planos() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1>Página de Planos 🚀</h1>
+    </div>
+  )
+}
