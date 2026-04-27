04c5693e94d55d3d682aff8ed67f9b98c735aa16
Ana Artagaveytia <anaartagaveytia1@gmail.com>
2026-04-27 18:45:09 -0300

    add plano de ação

---
+++ app/plano-acao/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+export default function PlanoAcao() {
+
+  return (
+    <div className="min-h-screen bg-[#020617] text-white p-6">
+
+      <h1 className="text-2xl font-bold mb-6">
+        Plano de Ação
+      </h1>
+
+    </div>
+  )
+}
